146ffc1afaffc09efa0a8baee1731a6264d393bc
lzlocki <lzlocki@poczta.fm>
2023-10-06 18:20:23 +0200

    Add testing methods responsible for creation of daily reports

--- ReportServiceTests.cs
+++ ReportServiceTests.cs
@@ -14,13 +14,7 @@ public class ReportServiceTests
             new string[] {"ee", "2022-02-22", "gg", "hh"},
             new string[] {"ii", "2022-03-04", "kk", "ll"}
             };
-/*
-        List<string[]> TestSet = new List<string[]>{
-            new string[] {"aa", "2022-02-21 14:52:48" , "cc", "dd"},
-            new string[] {"ee", "2022-02-22 00:00:00", "gg", "hh"},
-            new string[] {"ii", "2022-03-04 14:52:48", "kk", "ll"}
-            };
-*/
+
         List<string> expected = new List<string> {
             "2022-02-21",
             "2022-02-22",
@@ -40,10 +34,10 @@ public class ReportServiceTests
 
         // Arrange
         List<string[]> TestSet = new List<string[]>{
-            new string[] {"2022-02-21", "40", "10" },
-            new string[] {"2022-02-21", "40", "5" },
-            new string[] {"2022-03-04", "40", "100" },
-            new string[] {"2022-02-21", "20", "3" }
+            new string[] {"2022-02-21", "1000-1100", "40", "10" },
+            new string[] {"2022-02-21", "1200-1300", "40", "5" },
+            new string[] {"2022-03-04", "1400-1500", "40", "100" },
+            new string[] {"2022-02-21", "1500-1600", "20", "3" }
             };
             
         // Act
@@ -57,4 +51,33 @@ public class ReportServiceTests
         Assert.Equal("0.18", actual.PlanedPercentage);
     }
 
+    // [06.10.2023] Done
+    [Fact]
+    public void GetListOfDailyReportsBaseOnDataset_ReturnsListOfDailyReportsForGivenDataset() {
+        // Arrange
+        List<string[]> TestSet = new List<string[]>{
+            new string[] {"2022-02-21", "1000-1100", "40", "10" },
+            new string[] {"2022-02-21", "1200-1300", "40", "5" },
+            new string[] {"2022-02-21", "1500-1600", "20", "3" },
+            new string[] {"2022-03-04", "1400-1500", "40", "40" },
+            new string[] {"2022-03-04", "1600-1600", "20", "5" },
+            new string[] {"2022-03-04", "1700-1800", "40", "20" }
+            };
+
+        // Act
+        ReportService service = new();
+        List<ReportDaily> actual = service.GetListOfDailyReportsBaseOnDataset(TestSet, 0);
+
+        // Assert
+        // first object
+        Assert.Equal("2022-02-21", actual[0].Date);
+        Assert.Equal("100",  actual[0].PlannedOutput);
+        Assert.Equal("18",  actual[0].RealOutput);
+        Assert.Equal("0.18",  actual[0].PlanedPercentage);
+        // second object
+        Assert.Equal("2022-03-04", actual[1].Date);
+        Assert.Equal("100",  actual[1].PlannedOutput);
+        Assert.Equal("65",  actual[1].RealOutput);
+        Assert.Equal("0.65",  actual[1].PlanedPercentage);
+    }
 }
